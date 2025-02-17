ccfad2e668a7796b6993b79af075118837bc4110
Cliff Hunt <honduq@gmail.com>
2025-02-17 14:05:54 -0500

    added horse post route to write to console

--- haec/src/horse/controllers/horse.controller.ts
+++ haec/src/horse/controllers/horse.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { HorseService } from '../services/horse.service';
 
 @Controller('horses')
 export class HorseController {
 
-    constructor(private readonly horseService:HorseService){}
+  constructor(private readonly horseService:HorseService){}
 
-    @Get()
-    public async findAll() {
-        return this.horseService.getHorses();
+  @Get()
+  public async findAll() {
+    return this.horseService.getHorses();
   }
+
+  @Post()
+  handlePost(@Body() body: any): string {
+  return this.horseService.writeJSONBody(body);
+}
+
 }
--- haec/src/horse/services/horse.service.ts
+++ haec/src/horse/services/horse.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Request, Response, NextFunction, request } from 'express';
 
 @Injectable()
 export class HorseService {
     public async getHorses() {
         return "This is a string";
+    }
+
+    public writeJSONBody(data: any) {
+        console.log('Received data in the service:', data);
+        return 'Data logged successfully';
     }
 }
