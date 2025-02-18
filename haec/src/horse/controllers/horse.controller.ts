c0a00a4abef05aafe9eb6e7600e13b4507c8f2d4
Cliff Hunt <honduq@gmail.com>
2025-02-18 14:28:49 -0500

    added delete and softDelete

--- haec/src/horse/controllers/horse.controller.ts
+++ haec/src/horse/controllers/horse.controller.ts
@@ -3,15 +3,27 @@ import { HorseService } from '../services/horse.service';
 
 @Controller('horses')
 export class HorseController {
-  constructor(private readonly horseService: HorseService) {}
+  constructor(private readonly horseService: HorseService) { }
 
   @Get()
   public async findAll() {
     return this.horseService.getHorses();
   }
 
-  @Post()
-  handlePost(@Body() body: any): string {
-    return this.horseService.writeJSONBody(body);
+  @Post('/create')
+  async create(@Body() body: { showName: string; name: string; feed: string }) {
+    return this.horseService.createHorse(body);
+  }
+
+  @Post('/delete')
+  async delete(@Body() data: { id: number }) {
+    const horseID = data.id;
+    return await this.horseService.deleteHorse(horseID);
+  }
+
+  @Post('/softDelete')
+  async softDelete(@Body() data: { id: number }) {
+    const horseID = data.id;
+    return await this.horseService.softDeleteHorse(horseID);
   }
 }
--- haec/src/horse/services/horse.service.ts
+++ haec/src/horse/services/horse.service.ts
@@ -1,16 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 import { PrismaService } from '../../prisma.service';
+import { JsonObject } from '@prisma/client/runtime/library';
+import { ParamsTokenFactory } from '@nestjs/core/pipes';
 
 @Injectable()
 export class HorseService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) { }
 
   public async getHorses() {
-    return this.prismaService.horse.findMany();
+    const horses = await this.prismaService.horse.findMany();
+    return horses;
   }
 
-  public writeJSONBody(data: any) {
-    console.log('Received data in the service:', data);
-    return 'Data logged successfully';
+  public createHorse(data: { showName: string; name: string; feed: string }) {
+    return this.prismaService.horse.create({ data });
   }
+
+  public async deleteHorse(id: number): Promise<number> {
+    const deleteHorse = await this.prismaService.horse.delete({ where: { id: id }, });
+    console.log(deleteHorse);
+    return id;
+  }
+
+
+  public async softDeleteHorse(id: number): Promise<number> {
+    const softDeleteHorse = await this.prismaService.horse.update({
+      where: { id: id },
+      data: { deletedAt: new Date() }
+    }
+    );
+    console.log(softDeleteHorse);
+    return id;
+  }
+
+
 }
